7915cb6490e355478d38f338f1009e26e376aaea
ToastedThomas <blaketeahelser@gmail.com>
2024-12-18 21:06:51 -0600

    code rework
    a major javascript rework, adjusting CSS to fit the theme of my other projects, additional information is now displayed for each pokemon such as cry audio, available moves, and games the pokemon are included in.

--- script.js
+++ script.js
@@ -1,176 +1,204 @@
+const cardContainer = document.getElementById("container");
 
-fetch('https://pokeapi.co/api/v2/pokemon/?limit=250', {cache: "force-cache"})//grabs basic pokemon info including name and url for more info
-  .then( response => {
-    return response.json();//converts raw json into readable format
-  }).then( json => {
-    console.log(json);
-    displayUsers(json);//passes name and url info into function below
-  })
+const pokemonUrl = "https://pokeapi.co/api/v2/pokemon/";
+const pokemonLimit = 50;
+let offset = 0;
+let currentInfo = [];
 
-function displayUsers(info) {
-  info.results.forEach(element => {//loops through each pokemon loaded above and creates elements to house information about pokemon, then requests more info about each pokemon below
-    var img = document.createElement('img');
-    const box = document.createElement('div');
-    const typeBox = document.createElement('div');
-    var idNum = document.createElement('p');
-    const idNumBox = document.createElement('div');
-    var name = document.createElement('p');
-    const nameBox = document.createElement('div');
-    nameBox.setAttribute('id', 'nameBox');
-    idNumBox.setAttribute('id', 'idNumBox');
-    typeBox.setAttribute('id', 'typeBox');
-    nameBox.appendChild(name);
-    idNumBox.appendChild(idNum);
-    box.appendChild(img);
-    box.appendChild(nameBox);
-    box.appendChild(idNumBox);
-    box.appendChild(typeBox);
-    fetch(element.url, {cache: "force-cache"})//grabs more specific info for each pokemon
-      .then( response => {
-        return response.json();
-      }).then( json => {//takes current readable info about pokemon and uses it
-        img.src = json.sprites.front_default;
-        if (json.id < 10) {//takes pokemon id number and makes it look uniform
-          idNum.innerHTML = '#00'+json.id;
-        } else if (10 <= json.id && json.id < 100) {
-          idNum.innerHTML = '#0'+json.id;
-        } else {
-          idNum.innerHTML = '#'+json.id;
-        }
-        box.setAttribute('id', json.id);//assigns pokemon id number to the id of the element for reference later on
-        json.types.forEach(element => {//loops through each type a pokemon has 
-          var typeImg = document.createElement('img');
-          typeImg.classList.add('typeIcon');
-          typeImg.src = 'images/'+element.type.name+'.png';
-          typeBox.appendChild(typeImg);
-        })
-      })
-    box.classList.add('infoBox');
-    img.classList.add('pokeSprite');
-    name.innerHTML = element.name;
-    document.getElementById('container').appendChild(box);
-    box.onclick = function() {
-     expandInfo(box);
-    }
-    console.log(box);
+const fetchPokemonList = async () => {
+  try {
+    const res = await fetch(`${pokemonUrl}?limit=${pokemonLimit}&offset=${offset}`, { cache: "force-cache" }); // grabs list of pokemon names and urls for specific information
+    const data = await res.json();
+    console.log(data.results);
+    fetchPokemonSpecifics(data.results); //passes in an array of pokemon based on limit and offset in fetch request
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fetchPokemonList()
+
+
+const fetchPokemonSpecifics = async (pokemonArr) => {
+
+  currentInfo = await Promise.all(pokemonArr.map(item => fetch(item.url, { cache: "force-cache"}).then(res => res.json() )));
+  //console.log(currentInfo);
+
+  currentInfo.forEach(pokemon => {
+    const card = document.createElement("div");  //creating a div element manually / addEventListener would not work with template literal
+    card.id = pokemon.id;
+    card.classList.add("infoBox");
+
+    const mainContent = document.createElement("div");
+    mainContent.id = `main${pokemon.id}`;
+    mainContent.classList.add("mainContent");
+
+    //placing the content within another div below makes it easier to move and add content when expanding the card
+    mainContent.innerHTML += `
+        <p class="name">${pokemon.name}</p>
+        <img id="img${pokemon.id}" src="${pokemon.sprites.front_default}" />
+        <p>#${pokemon.id}</p>
+    `;
+
+    card.appendChild(mainContent);
+
+    const typesContainer = document.createElement("div");
+
+    pokemon.types.forEach(type => { // adding type images at bottom of each card
+      typesContainer.innerHTML += `
+        <img class="cardTypes" src="./images/${type.type.name}.png" />
+      `;
+    });
+
+    mainContent.appendChild(typesContainer);
+
+    cardContainer.appendChild(card); //adding card to main container
+
+    card.addEventListener("click",  () => { //div's click event, passing along pokemon's entire object information
+      if (card.className === "infoBox") {
+        expandCard(pokemon, card);
+      } 
+    });
   });
+  
+
 }
 
-function expandInfo(box) {
-  if (!document.getElementById('container').querySelector('.expandedBox')) {
-    var closeButton = document.createElement('img');
-    var nameTag = document.createElement('p');
-    var idTag = document.createElement('p');
-    var typesTag = document.createElement('p');
-    var heightInfo = document.createElement('p');
-    var weightInfo = document.createElement('p');
-    var heightTag = document.createElement('p');
-    var weightTag = document.createElement('p');
-    var abilityTag = document.createElement('h5');
-    var statsTag = document.createElement('h5');
-    var hiddenTag = document.createElement('h6');
-    const heightBox = document.createElement('div');
-    const weightBox = document.createElement('div');
-    const abilityBox = document.createElement('div');
-    const statsBox = document.createElement('div');
-    heightBox.setAttribute('id', 'heightBox');
-    weightBox.setAttribute('id', 'weightBox');
-    abilityBox.setAttribute('id', 'abilityBox');
-    statsBox.setAttribute('id', 'statsBox');
-    nameTag.innerHTML = 'Name';
-    idTag.innerHTML = 'Id no.';
-    typesTag.innerHTML = 'Type(s)';
-    heightTag.innerHTML = 'Height';
-    weightTag.innerHTML = 'Weight';
-    abilityTag.innerHTML = 'Abilities';
-    hiddenTag.innerHTML = 'Hidden Ability';
-    statsTag.innerHTML = 'Stats';
-    closeButton.src = 'images/closeButton.png';
-    closeButton.classList.add('closeButton');
-    heightBox.appendChild(heightTag);
-    weightBox.appendChild(weightTag);
-    abilityBox.appendChild(abilityTag);
-    heightBox.appendChild(heightInfo);
-    weightBox.appendChild(weightInfo);
-    statsBox.appendChild(statsTag);
-    box.appendChild(closeButton);
-    box.insertBefore(weightBox, box.childNodes[3]);
-    box.insertBefore(heightBox, box.childNodes[3]);
-    box.appendChild(abilityBox);
-    box.appendChild(statsBox);
-    box.classList.remove('infoBox');
-    box.classList.add('expandedBox');
-    box.firstChild.classList.remove('pokeSprite');
-    box.firstChild.classList.add('bigPokeSprite');
-    box.querySelector('#typeBox').style.width = '20%';
-    box.querySelector('#typeBox').style.margin = '0px 10px 0px auto';
-    box.querySelector('#typeBox').style.justifyContent = 'space-between';
-    box.querySelector('#typeBox').insertBefore(typesTag, box.querySelector('#typeBox').childNodes[0]);
-    box.querySelector('#nameBox').classList.add('nameBox');
-    box.querySelector('#nameBox').insertBefore(nameTag, box.querySelector('#nameBox').childNodes[0]);
-    box.querySelector('#idNumBox').classList.add('idBox');
-    box.querySelector('#idNumBox').insertBefore(idTag, box.querySelector('#idNumBox').childNodes[0]);
-    fetch('https://pokeapi.co/api/v2/pokemon/'+box.id, {cache: "force-cache"})
-    .then( response => {
-      return response.json();//converts raw json into readable format
-    }).then( json => {
-      heightInfo.innerHTML = (json.height / 10).toFixed(1) + ' m';
-      weightInfo.innerHTML = (json.weight / 10).toFixed(1) + ' kg';
-      json.abilities.forEach(element => {
-        var abilityInfo = document.createElement('p');
-        abilityInfo.innerHTML = element.ability.name;
-        abilityBox.appendChild(abilityInfo);
-        if (element.is_hidden) {
-          abilityInfo.appendChild(hiddenTag);
-        }
-      })
-      json.stats.forEach(element => {
-        const statHold = document.createElement('div');
-        var statName = document.createElement('p');
-        var statNum = document.createElement('p');
-        statHold.classList.add('smallStatBox');
-        statHold.setAttribute('id', element.stat.name);
-        statNum.innerHTML = element.base_stat;
-        if (element.stat.name === 'hp') {
-          statName.innerHTML = 'Hp';
-        } else if (element.stat.name === 'attack') {
-          statName.innerHTML = 'Atk';
-        } else if (element.stat.name === 'defense') {
-          statName.innerHTML = 'Def';
-        } else if (element.stat.name === 'special-attack') {
-          statName.innerHTML = 'SpA';
-        } else if (element.stat.name === 'special-defense') {
-          statName.innerHTML = 'SpD';
-        } else if (element.stat.name === 'speed') {
-          statName.innerHTML = 'Spe';
-        }
-        statHold.appendChild(statName);
-        statHold.appendChild(statNum);
-        statsBox.appendChild(statHold);
-        console.log(element)
-      })
-      console.log(json);
-    })
-  closeButton.onclick = function() { 
-    box.removeChild(closeButton);
-    box.removeChild(heightBox);
-    box.removeChild(weightBox);
-    box.removeChild(abilityBox);
-    box.removeChild(statsBox);
-    box.classList.remove('expandedBox');
-    box.classList.add('infoBox');
-    box.firstChild.classList.remove('bigPokeSprite');
-    box.firstChild.classList.add('pokeSprite');
-    box.querySelector('#nameBox').classList.remove('nameBox');
-    box.querySelector('#nameBox').removeChild(box.querySelector('#nameBox').childNodes[0]);
-    box.querySelector('#idNumBox').classList.remove('idBox');
-    box.querySelector('#idNumBox').removeChild(box.querySelector('#idNumBox').childNodes[0]);
-    box.querySelector('#typeBox').removeChild(box.querySelector('#typeBox').childNodes[0]);
-    box.querySelector('#typeBox').lastChild.style.paddingRight = '0px';
-    box.querySelector('#typeBox').style.width = '50%';
-    box.querySelector('#typeBox').style.margin = '0px auto';
-    box.querySelector('#typeBox').style.justifyContent = 'space-around';
-    event.stopPropagation();//this line is very important, it prevents the mouse from 'clicking through' the close button, ensures that the box.onclick event doesn't fire again
-    console.log('close') }
-  }
+const expandCard = (pokemon, card) => {
+  card.classList.remove("infoBox");
+  card.classList.add("infoBoxExpanded");
+  console.log(pokemon, card);
+
+  //adding close btn
+  const closeBtn = document.createElement("img");
+  closeBtn.classList.add("closeBtn");
+  closeBtn.src = "./images/closeButton.png";
+  closeBtn.addEventListener("click", (event) => {
+    closeCard(card);
+    event.stopPropagation();//this prevents 'clicking through' the X and activating the expandCard function again
+  });
+  card.appendChild(closeBtn);
+
+  //adding additional pokemon info
+  const additionalInfo = document.createElement("div");
+  additionalInfo.classList.add("additionalInfo");
+  additionalInfo.innerHTML = `
+    <div>
+      <label for="sprites${pokemon.id}">Sprites: </label>
+      <select id="sprites${pokemon.id}">
+        <option value="${pokemon.sprites.front_default}">Front Default</option>
+        <option value="${pokemon.sprites.front_shiny}">Front Shiny</option>
+        <option value="${pokemon.sprites.back_default}">Back Default</option>
+        <option value="${pokemon.sprites.back_shiny}">Back Shiny</option>
+      </select>
+    </div>
+    <div>
+      <p>Height: ${pokemon.height}</p>
+      <p>Weight: ${pokemon.weight}</p>
+      <p>Base Experience: ${pokemon.base_experience}xp</p>
+    </div>
+  `;
+  card.appendChild(additionalInfo);
+  const selection = document.getElementById(`sprites${pokemon.id}`);
+  selection.addEventListener("change", () => {
+    document.getElementById(`img${pokemon.id}`).src = selection.value;
+  })
+
+  //adding potential abilities
+  const abilities = document.createElement("div");
+  abilities.classList.add("abilities");
+  const abilityList = pokemon.abilities;
+  abilityList.forEach(ability => {
+    if (ability.is_hidden) {
+      abilities.innerHTML += `
+        <div>
+          <p>${ability.ability.name}</p>
+          <p class="hiddenAbility">(Hidden)</p>
+        </div>
+      `;
+    } else {
+      abilities.innerHTML += `
+        <div>
+          <p>${ability.ability.name}</p>
+        </div>
+      `
+    }
+  })
+  card.appendChild(abilities);
+
+  //adding games list
+  const games = document.createElement("div");
+  games.classList.add("games");
+  const gamesList = pokemon.game_indices;
+  gamesList.forEach(game => {
+    games.innerHTML += `
+    <p>${game.version.name}</p>
+    `;
+  });
+  card.appendChild(games);
+
+  //adding available moves list
+  const moves = document.createElement("div");
+  moves.classList.add("moves");
+  const movesList = pokemon.moves;
+  movesList.forEach(move => {
+    moves.innerHTML += `
+      <p>${move.move.name}</p>
+    `;
+  });
+  card.appendChild(moves);
+
+  //adding base stats
+  const stats = document.createElement("div");
+  stats.classList.add("stats");
+  stats.innerHTML = `
+    <div class="hp">
+      <p>HP</p>
+      <p>${pokemon.stats[0].base_stat}</p>
+    </div>
+    <div class="attack">
+      <p>Att</p>
+      <p>${pokemon.stats[1].base_stat}</p>
+    </div>
+    <div class="defence">
+      <p>Def</p>
+      <p>${pokemon.stats[2].base_stat}</p>
+    </div>
+    <div class="spAttack">
+      <p>Sp. Att</p>
+      <p>${pokemon.stats[3].base_stat}</p>
+    </div>
+    <div class="spDefence">
+      <p>Sp. Def</p>
+      <p>${pokemon.stats[4].base_stat}</p>
+    </div>
+    <div class="speed">
+      <p>Speed</p>
+      <p>${pokemon.stats[5].base_stat}</p>
+    </div>
+  `;
+  card.appendChild(stats);
+
+  //adding cry sounds
+  const cries = document.createElement("div");
+  cries.classList.add("cries");
+  cries.innerHTML = `
+    <audio controls>
+      <source src="${pokemon.cries.latest}" type="audio/ogg">
+    </audio>
+  `;
+  card.appendChild(cries);
+}
+
+const closeCard = card => {
+  const children = new Array(...card.children); //creates an array of all child elements from card
+
+  children.forEach(child => { //loops through all children, if child is not mainContent then delete
+    if (child.className != "mainContent") {
+      child.remove();
+    }
+  })
+
+  card.classList.remove("infoBoxExpanded"); //return card to normal state
+  card.classList.add("infoBox");
 }
