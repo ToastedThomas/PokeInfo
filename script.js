ec5e98af88f9ff8acf352f760c19582a06e7c54f
ToastedThomas <blaketeahelser@gmail.com>
2024-12-19 23:58:30 -0600

    Styling and new feature
    Additional styling within the expanded cards, each section now has a small header element explaining what each section contains. Added pokemon types listed in plaintext in the expanded cards as well as styling audio controls in browsers that support it. There is also pagination fully implemented, meaning you can now browse different pages of pokemon, currently, 1302 pokemon are listed in the PokeAPI.

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const fetchPokemonList = async () => {
     const data = await res.json();
     console.log(data.results);
     fetchPokemonSpecifics(data.results); //passes in an array of pokemon based on limit and offset in fetch request
+    createPaginationControls();//updates page controls
   } catch (err) {
     console.log(err);
   }
@@ -20,9 +21,9 @@ fetchPokemonList()
 
 
 const fetchPokemonSpecifics = async (pokemonArr) => {
+  cardContainer.innerHTML = ""; //clears content when loading new data
 
   currentInfo = await Promise.all(pokemonArr.map(item => fetch(item.url, { cache: "force-cache"}).then(res => res.json() )));
-  //console.log(currentInfo);
 
   currentInfo.forEach(pokemon => {
     const card = document.createElement("div");  //creating a div element manually / addEventListener would not work with template literal
@@ -69,44 +70,53 @@ const expandCard = (pokemon, card) => {
   card.classList.add("infoBoxExpanded");
   console.log(pokemon, card);
 
-  //adding close btn
-  const closeBtn = document.createElement("img");
-  closeBtn.classList.add("closeBtn");
-  closeBtn.src = "./images/closeButton.png";
-  closeBtn.addEventListener("click", (event) => {
-    closeCard(card);
-    event.stopPropagation();//this prevents 'clicking through' the X and activating the expandCard function again
-  });
-  card.appendChild(closeBtn);
-
   //adding additional pokemon info
   const additionalInfo = document.createElement("div");
+  additionalInfo.id = `additionalInfo${pokemon.id}`;
   additionalInfo.classList.add("additionalInfo");
   additionalInfo.innerHTML = `
-    <div>
-      <label for="sprites${pokemon.id}">Sprites: </label>
-      <select id="sprites${pokemon.id}">
-        <option value="${pokemon.sprites.front_default}">Front Default</option>
-        <option value="${pokemon.sprites.front_shiny}">Front Shiny</option>
-        <option value="${pokemon.sprites.back_default}">Back Default</option>
-        <option value="${pokemon.sprites.back_shiny}">Back Shiny</option>
-      </select>
-    </div>
-    <div>
-      <p>Height: ${pokemon.height}</p>
-      <p>Weight: ${pokemon.weight}</p>
-      <p>Base Experience: ${pokemon.base_experience}xp</p>
-    </div>
+      <div>
+        <label for="sprites${pokemon.id}">Sprites: </label>
+        <select id="sprites${pokemon.id}">
+          <option value="${pokemon.sprites.front_default}">Front Default</option>
+          <option value="${pokemon.sprites.front_shiny}">Front Shiny</option>
+          <option value="${pokemon.sprites.back_default}">Back Default</option>
+          <option value="${pokemon.sprites.back_shiny}">Back Shiny</option>
+        </select>
+      </div>
+      <div>
+        <p>Height: ${pokemon.height}</p>
+        <p>Weight: ${pokemon.weight}</p>
+        <p>Base Experience: ${pokemon.base_experience}xp</p>
+      </div>
   `;
   card.appendChild(additionalInfo);
-  const selection = document.getElementById(`sprites${pokemon.id}`);
-  selection.addEventListener("change", () => {
-    document.getElementById(`img${pokemon.id}`).src = selection.value;
-  })
+  setTimeout(() => {//adding a 500ms delay on finding/adding eventlistener to element, to make sure the element is actually in the DOM before attempting to grab it
+    const selection = document.getElementById(`sprites${pokemon.id}`);
+    selection.addEventListener("change", () => {
+      document.getElementById(`img${pokemon.id}`).src = selection.value;
+    });
+  }, 500);
+
+  const typeDiv = document.createElement("div");
+  typeDiv.classList.add("types");
+  const typesList = pokemon.types;
+  typeDiv.innerHTML = `<span>Types: </span>`
+  typesList.forEach((type, i) => {
+    typeDiv.innerHTML += `<span>${type.type.name.toUpperCase()}</span>`;
+    if (i != typesList.length - 1) {
+      typeDiv.innerHTML += `<span>, </span>`;
+    };
+  });
+  document.getElementById(`additionalInfo${pokemon.id}`).appendChild(typeDiv);
+
 
   //adding potential abilities
   const abilities = document.createElement("div");
   abilities.classList.add("abilities");
+  abilities.innerHTML += `
+    <h4 class="sectionTitle">Potential Abilities</h4>
+  `;
   const abilityList = pokemon.abilities;
   abilityList.forEach(ability => {
     if (ability.is_hidden) {
@@ -129,30 +139,42 @@ const expandCard = (pokemon, card) => {
   //adding games list
   const games = document.createElement("div");
   games.classList.add("games");
+  games.innerHTML += `
+    <h4 class="sectionTitle">Games found in</h4>
+  `;
   const gamesList = pokemon.game_indices;
-  gamesList.forEach(game => {
+  gamesList.forEach((game, i) => {
     games.innerHTML += `
     <p>${game.version.name}</p>
     `;
+    if (i != gamesList.length - 1) {
+      games.innerHTML += `<hr>`;
+    };
   });
   card.appendChild(games);
 
   //adding available moves list
   const moves = document.createElement("div");
   moves.classList.add("moves");
+  moves.innerHTML += `
+    <h4 class="sectionTitle">Available Moves</h4>
+  `;
   const movesList = pokemon.moves;
-  movesList.forEach(move => {
+  movesList.forEach((move, i) => {
     moves.innerHTML += `
       <p>${move.move.name}</p>
     `;
+    if (i != movesList.length - 1) {
+      moves.innerHTML += `<hr>`;
+    };
   });
   card.appendChild(moves);
 
   //adding base stats
-  const stats = document.createElement("div");
-  stats.classList.add("stats");
-  stats.innerHTML = `
-    <div class="hp">
+  const stats = `
+    <div class="stats">
+    <h4 class="sectionTitle">Base Stats</h4>
+      <div class="hp">
       <p>HP</p>
       <p>${pokemon.stats[0].base_stat}</p>
     </div>
@@ -176,18 +198,31 @@ const expandCard = (pokemon, card) => {
       <p>Speed</p>
       <p>${pokemon.stats[5].base_stat}</p>
     </div>
+    </div>
   `;
-  card.appendChild(stats);
+  card.innerHTML += stats;
 
   //adding cry sounds
-  const cries = document.createElement("div");
-  cries.classList.add("cries");
-  cries.innerHTML = `
-    <audio controls>
-      <source src="${pokemon.cries.latest}" type="audio/ogg">
-    </audio>
+  const cries = `
+    <div class="cries">
+    <h4 class="sectionTitle">Cry Sound</h4>
+      <audio controls>
+        <source src="${pokemon.cries.latest}" type="audio/ogg">
+      </audio>
+    </div>
   `;
-  card.appendChild(cries);
+  card.innerHTML += cries;
+
+  //adding close btn
+  const closeBtn = `
+    <img id="closeBtn" src="./images/closeButton.png" />
+  `;
+  card.innerHTML += closeBtn;
+  const closeBtnImg = document.getElementById("closeBtn");
+  closeBtnImg.addEventListener("click", (event) => {
+    closeCard(card);
+    event.stopPropagation();//this prevents 'clicking through' the X and activating the expandCard function again
+  });
 }
 
 const closeCard = card => {
@@ -201,4 +236,24 @@ const closeCard = card => {
 
   card.classList.remove("infoBoxExpanded"); //return card to normal state
   card.classList.add("infoBox");
+}
+
+const createPaginationControls = () => {
+  paginationContainer.innerHTML = ""; // Clear previous pagination controls
+  const totalPages = Math.ceil(1302 / pokemonLimit);
+  for (let i = 1; i <= totalPages; i++) {
+    const button = document.createElement("button");
+    button.textContent = i;
+    if ((i - 1) * pokemonLimit === offset) {
+      button.classList.add("active"); // Highlight the active page
+      button.disabled = true;
+    }
+    button.addEventListener("click", () => changePage(i));
+    paginationContainer.appendChild(button);
+  }
+};
+
+const changePage = pageNumber => {
+  offset = (pageNumber - 1) * pokemonLimit;
+  fetchPokemonList();
 }
